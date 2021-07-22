a23bc59b3e9bf89096cb812c7af8a7dbf470c7a4
marzzy mogh <marzzy.m@gmail.com>
2021-07-22 18:42:53 +0430

    feat: add breakpoint

--- theme.ts
+++ theme.ts
@@ -1,18 +1,49 @@
 import ThemeType from 'types/theme';
 
+// TODO: work on pallete
+// const colors = {
+//   dark: {
+//     main: { text: '#ff914d', bg: '#344f5c' },
+//     neutral: { text: '', bg: '' },
+//     accent: { text: '#ffffff', bg: '#ff914d' },
+//   },
+//   light: {
+//     main: { text: '#ff914d', bg: '#f6f1ee' },
+//   },
+// };
+
 const colors = {
   dark: {
-    primary: 'black',
+    primary: '#000',
   },
   light: {
-    primary: 'white',
+    primary: '#fff',
   },
+};
+
+const BrValues = {
+  xs: 300,
+  sm: 600,
+  md: 960,
+  lg: 1280,
 };
 
 function theme(paletteType: 'dark'|'light'): ThemeType {
   return {
     palette: {
       ...colors[paletteType],
+    },
+    breakpoints: {
+      values: BrValues,
+      up(brPoint:string) {
+        return `@media only screen and (min-width: ${BrValues[brPoint]}px)`;
+      },
+      down(brPoint:string) {
+        return `@media only screen and (max-width: ${BrValues[brPoint]}px)`;
+      },
+      between(startBrPoint: string, endBrPoint: string) {
+        return `@media only screen and (min-width:${BrValues[startBrPoint]}px) and (max-width:${BrValues[endBrPoint]}px)`;
+      },
     },
   };
 }
